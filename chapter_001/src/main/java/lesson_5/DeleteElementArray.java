0aafb92ad3c9d2ae72294814f4c3d2dd3fd48836
Alexandr Smirnov <alexandrsmi02@mail.ru>
2017-01-22 17:22:44 +0300

    update

---
+++ chapter_001/src/main/java/lesson_5/DeleteElementArray.java
@@ -0,0 +1,61 @@
+package lesson_5;
+import java.util.Arrays;
+
+	/**
+	* Тест класса удаляющего дублирующиеся элементы массива.
+	* @author smirnov
+	* @since 22.01.2017
+	*/
+ 
+public class DeleteElementArray {
+
+	/**
+	* Метод присваивает дублирующимся элементам - null.
+	*/
+   
+    public String[] allocNullToDuplicatingElements(String[] strings) {
+        for (int i = strings.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (strings[i] == strings[j]) {
+                    strings[j] = null;
+                }
+            }
+        }
+        return strings;
+    }
+	
+	/**
+	* Метод переносит null элементы в конец массива.
+	*/
+	
+    public String[] sortNullTheElementsArray(String[] strings) {
+        int count = 0;
+        for (int i = strings.length - 1; i >= count; ) {
+            if (strings[count] == null) {
+                String tem;
+                tem = strings[i];
+                strings[i] = strings[count];
+                strings[count] = tem;
+                i--;
+            }
+            count++;
+        }
+        return strings;
+    }
+	
+	/**
+	* Метод удаляет null значение из массива.
+	*/
+	
+    public String[] deletionNullInArray(String[] strings) {
+        int countNull = 0;
+        for (String ar : strings) {
+            if (ar == null) {
+                countNull++;
+            }
+        }
+        String[] arrayCopy = Arrays.copyOf(strings, (strings.length) - countNull);
+
+        return arrayCopy;
+    }
+}
---
+++ chapter_001/src/test/java/DeleteElementsArrayTest.java
@@ -0,0 +1,95 @@
+import org.junit.Assert;
+import org.junit.Test;
+import lesson_5.DeleteElementArray;
+
+/**
+ * Тест класса удаляющего дублирующиеся элементы массива.
+ * @author smirnov
+ * @since 22.01.2017
+ */
+public class DeleteElementsArrayTest {
+
+	/**
+	 * Тест метода присвающего дублирующим элементы массива null.
+	 * Тест проверяет массив с не четным количеством элеметнов.
+	 */
+
+    @Test
+    public void allocNullToDuplicatingElementsDontEvenArrayTest() {
+        DeleteElementArray deleteElementArray = new DeleteElementArray();
+        String[] arrayTest = {"4", "4", "3"};
+        String[] arrayResultTest = {null, "4", "3"};
+        String[] result = deleteElementArray.allocNullToDuplicatingElements(arrayTest);
+        Assert.assertArrayEquals(arrayResultTest, result);
+    }
+
+	/**
+	 * Тест метода переносящего null элементы в конец массива.
+	 * Тест проверяет массив с не четным количеством элеметнов.
+	 */
+	 
+    @Test
+    public void sortNullTheElementsDontEvenArrayTest() {
+        DeleteElementArray deleteElementArray = new DeleteElementArray();
+        String[] arrayTest = {null, "4", "3"};
+        String[] arrayResultTest = {"3", "4", null};
+        String[] result = deleteElementArray.sortNullTheElementsArray(arrayTest);
+        Assert.assertArrayEquals(arrayResultTest, result);
+    }
+	
+	/**
+	 * Тест метода удаляющего null элементы массива.
+	 * Тест проверяет массив с не четным количеством элеметнов.
+	 */
+
+    @Test
+    public void deletionNullInArrayDontEvenArray() {
+        DeleteElementArray deleteElementArray = new DeleteElementArray();
+        String[] arrayTest = {"3","4",null};
+        String[] arrayResultTest = {"3","4"};
+        String[] result = deleteElementArray.deletionNullInArray(arrayTest);
+        Assert.assertArrayEquals(arrayResultTest, result);
+    }
+	
+	/**
+	 * Тест метода присвающего дублирующим элементы массива null.
+	 * Тест проверяет массив с четным количеством элеметнов.
+	 */
+	 
+    @Test
+    public void allocNullToDuplicatingElementsEvenArrayTest() {
+        DeleteElementArray deleteElementArray = new DeleteElementArray();
+        String[] arrayTest = {"4", "4", "3","5"};
+        String[] arrayResultTest = {null, "4", "3","5"};
+        String[] result = deleteElementArray.allocNullToDuplicatingElements(arrayTest);
+        Assert.assertArrayEquals(arrayResultTest, result);
+    }
+	
+	/**
+	 * Тест метода переносящего null элементы в конец массива.
+	 * Тест проверяет массив с не четным количеством элеметнов.
+	 */
+
+    @Test
+    public void sortNullTheElementsDontArrayTest() {
+        DeleteElementArray deleteElementArray = new DeleteElementArray();
+        String[] arrayTest = {null, "4", "3","5"};
+        String[] arrayResultTest = {"5", "4","3", null};
+        String[] result = deleteElementArray.sortNullTheElementsArray(arrayTest);
+        Assert.assertArrayEquals(arrayResultTest, result);
+    }
+
+	/**
+	 * Тест метода удаляющего null элементы массива.
+	 * Тест проверяет массив с не четным количеством элеметнов.
+	 */
+	 
+    @Test
+    public void deletionNullInArrayEvenArray() {
+        DeleteElementArray deleteElementArray = new DeleteElementArray();
+        String[] arrayTest = {"5","3","4",null};
+        String[] arrayResultTest = {"5","3","4"};
+        String[] result = deleteElementArray.deletionNullInArray(arrayTest);
+        Assert.assertArrayEquals(arrayResultTest, result);
+    }
+}
