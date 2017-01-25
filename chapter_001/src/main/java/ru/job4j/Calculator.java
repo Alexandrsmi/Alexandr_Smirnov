b86f54c7c2e68d03d1ac34507c3cf1ab87988ed9
Alexandr Smirnov <alexandrsmi02@mail.ru>
2017-01-25 12:06:49 +0300

    update

--- chapter_001/src/main/java/ru/job4j/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/Calculator.java
@@ -2,16 +2,18 @@ package ru.job4j;
 
 /**
  * Класс выводящий "Hello world".
+ *
  * @author smirnov
- * @since 20.07.2017
  * @version 1
+ * @since 20.07.2017
  */
 public class Calculator {
-	/**
-	 * Вывод строки в консоль.
-	 * @param arg - arg.
-	 */
-	public static void main(String[] arg) {
-		System.out.println("Hello world");
-	}
+    /**
+     * Вывод строки в консоль.
+     *
+     * @param arg - arg.
+     */
+    public static void main(String[] arg) {
+        System.out.println("Hello world");
+    }
 }
---
+++ chapter_002/tracker/src/main/java/tracker.java
@@ -0,0 +1,9 @@
+/**
+ * Class
+ *
+ * @author Aleksandr Smirnov.
+ * @vesion
+ * @since 25.01.2017.
+ */
+public class tracker {
+}
